8d1666d78fe2c9ea5703c46ca6ddd20f6c128e78
Sumit Bose <41384276+BOSE-git@users.noreply.github.com>
2023-06-02 17:34:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+document.querySelector(".b1").addEventListener("click", (event) =>{
+    var audio = new Audio('b1.mp3');
+    audio.play();
+});
+document.querySelector(".b2").addEventListener("click", (event) =>{
+    var audio = new Audio('b2.mp3');
+    audio.play();
+});
+document.querySelector(".b3").addEventListener("click", (event) =>{
+    var audio = new Audio('b3.mp3');
+    audio.play();
+});
+document.querySelector(".b4").addEventListener("click", (event) =>{
+    var audio = new Audio('b4.mp3');
+    audio.play();
+});
+document.querySelector(".b5").addEventListener("click", (event) =>{
+    var audio = new Audio('b5.mp3');
+    audio.play();
+});
+document.querySelector(".b6").addEventListener("click", (event) =>{
+    var audio = new Audio('b6.mp3');
+    audio.play();
+});
+document.querySelector(".b7").addEventListener("click", (event) =>{
+    var audio = new Audio('b7.mp3');
+    audio.play();
+});
+document.querySelector(".b8").addEventListener("click", (event) =>{
+    var audio = new Audio('b5.mp3');
+    audio.play();
+});
